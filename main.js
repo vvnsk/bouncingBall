ca7c8b8efc1764db0547d4303fc98d64f2a12f09
vvnsk <vvnsk92@gmail.com>
2015-09-11 22:37:30 +0530

    Base Version

---
+++ main.js
@@ -0,0 +1,126 @@
+var score = 0;
+var scoreText;
+
+// Define our main state
+var main = {
+  preload: function() {
+		// This function will be executed at the beginning     
+        // That's where we load the game's assets  
+		
+		// Load the paddle image
+		game.load.image('paddle', 'assets/paddle.png');
+		
+		// Load the brick sprite
+		game.load.image('brick', 'assets/brick.png');
+		
+		// Load the ball sprite
+		game.load.image('ball', 'assets/ball.png');
+
+  },
+
+  create: function() { 
+	  // This function is called after the preload function     
+	  // Here we set up the game, display sprites, etc.
+
+      game.stage.backgroundColor = '#212121';
+	
+	  // Initialize the physics system of the game
+	  game.physics.startSystem(Phaser.Physics.ARCADE);
+
+	  // Create a variable to handle the arrow keys
+	  this.cursor = game.input.keyboard.createCursorKeys();
+
+	  // Create the paddle at the bottom of the screen
+	  this.paddle = game.add.sprite(200, 400, 'paddle');
+
+	  // Enable the physics system for the paddle
+	  game.physics.arcade.enable(this.paddle);
+
+	  // Make sure the paddle won't move when hit by the ball
+	  this.paddle.body.immovable = true;
+	  
+	  //donot allow the paddle to go beyond the screen
+	  this.paddle.body.collideWorldBounds = true;
+	  
+	  // Create a group that will contain all the bricks
+	 this.bricks = game.add.group();
+	 this.bricks.enableBody = true;
+
+	 // Create the 16 bricks
+	 for (var i = 0; i < 5; i++)
+	   for (var j = 0; j < 5; j++)
+		 game.add.sprite(55+i*60, 55+j*35, 'brick', 0, this.bricks);
+
+	 // Make sure that the bricks won't move
+	 this.bricks.setAll('body.immovable', true);
+	 
+	 // Create the ball with physics
+	this.ball = game.add.sprite(200, 300, 'ball');
+	game.physics.arcade.enable(this.ball);
+
+	// Add velocity to the ball
+	this.ball.body.velocity.x = 200; 
+	this.ball.body.velocity.y = 200;
+	this.ball.body.rotation = 200;
+	//this.ball.body.angularVelocity = 200;
+
+	// Make the ball bouncy 
+	this.ball.body.collideWorldBounds = true;
+	this.ball.body.bounce.x = 1; 
+	this.ball.body.bounce.y = 1;
+	
+	//  The score
+    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#fff' });
+  },
+
+  update: function() {
+    // This function is called 60 times per second    
+        // It contains the game's logic  
+
+		// If the right arrow is pressed, move the paddle to the right
+	  if (this.cursor.right.isDown) 
+		this.paddle.body.velocity.x = 350;
+
+	  // If the left arrow if pressed, move left
+	  else if (this.cursor.left.isDown) 
+		this.paddle.body.velocity.x = -350;
+
+	  // If no arrow is pressed, stop moving
+	  else 
+		this.paddle.body.velocity.x = 0; 
+		
+	  // Make the paddle and the ball collide
+	  game.physics.arcade.collide(this.paddle, this.ball);
+
+	  // Call the 'hit' function when the ball hit a brick
+	  game.physics.arcade.collide(this.ball, this.bricks, this.hit, null, this);
+
+     //console.log();
+      if(this.ball.body.position.y>420){
+          alert("Game Over!!");
+          game.state.start('main');
+          score=0;
+      };
+	  
+	},
+  hit: function(ball, brick) {
+
+      // When the ball hits a brick, kill the brick
+      brick.kill();
+
+      //  Add and update the score
+      score += 10;//a=scoreText;
+      scoreText.text = 'Score: ' + score;
+
+      if(score==250){
+          alert("Congratulation!!!");
+          game.state.start('main');
+          score=0;
+      }
+  }
+};
+
+// Initialize Phaser, and start our 'main' state 
+var game = new Phaser.Game(400, 450, Phaser.AUTO, 'gameDiv');
+game.state.add('main', main);
+game.state.start('main');
